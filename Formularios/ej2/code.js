f668af667b01541e4b691f06081bba91c23ddf41
GuillermoModesto <guillermomodestoanti@gmail.com>
2024-11-24 13:38:14 +0100

    ejercicio

--- Formularios/ej2/code.js
+++ Formularios/ej2/code.js
@@ -1,3 +1,7 @@
 window.onload = function() {
-    
+    let formu = document.querySelector("form");
+    formu[0].oninput = function() {
+        console.log("hola")
+        formu[1].value = this.value;
+    }
 }
